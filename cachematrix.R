cb47bd3d257c1831657c97aa16868f06cf5e9913
Bilesh Ganguly <bilesh1991@yahoo.in>
2017-02-02 00:00:40 +0530

    A pair of functions that cache the inverse of a matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is usually a costly computation and there may be some benefit 
+## to caching the inverse of a matrix rather than computing it repeatedly.
+## Following is a pair of functions that cache the inverse of a matrix.
 
-## Write a short comment describing this function
-
+## makeCacheMatrix : This function creates a special "matrix" object that can 
+## cache its inverse. This function is really a list containing a function to
+## 1. set the value of the matrix
+## 2. get the value of the matrix
+## 3. set the value of the inverse of the matrix
+## 4. get the value of the inverse of the matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) inv <<- inverse
+  getInverse <- function() inv
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
+## cacheSolve : This function computes the inverse of the special "matrix" returned 
+## by makeCacheMatrix above. If the inverse has already been calculated (and the 
+## matrix has not changed), then cacheSolve should retrieve the inverse from the cache.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if (!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setInverse(inv)
+  inv
 }
+
+## > mat <- makeCacheMatrix(matrix(c(1,1,1,3,4,3,3,3,4),3,3))
+## > mat$get()
+## [,1] [,2] [,3]
+## [1,]    1    3    3
+## [2,]    1    4    3
+## [3,]    1    3    4
+## > cacheSolve(mat)
+## [,1] [,2] [,3]
+## [1,]    7   -3   -3
+## [2,]   -1    1    0
+## [3,]   -1    0    1
+## > cacheSolve(mat)
+## getting cached data
+## [,1] [,2] [,3]
+## [1,]    7   -3   -3
+## [2,]   -1    1    0
+## [3,]   -1    0    1
